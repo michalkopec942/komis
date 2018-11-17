0c29ca9a9c367451289bb153d05030bda6076e92
michalkopec942 <45126278+michalkopec942@users.noreply.github.com>
2018-11-17 18:12:30 +0100

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        private float fCenaZakupu;
+        private float fMarza;
+        private int iRokProdukcji;
+       
+
+        private float Marza
+        {
+            get
+            {
+                return fMarza;
+            }
+        }
+
+        private float CenaZakupu
+        {
+            get
+            {
+                return fCenaZakupu;
+            }
+        }
+
+        private int RokProdukcji
+        {
+            get
+            {
+                return iRokProdukcji;
+            }
+        }
+       void WyswietlRok()
+        {
+
+        }
+       float podajCenaKoncowa()
+        {
+            float cenakoncowa;
+            cenakoncowa = CenaZakupu + Marza;
+
+            return cenakoncowa;
+        }
+        public abstract string 
+
+
+
+    }
+
+}
+      
